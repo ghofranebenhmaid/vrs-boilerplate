6d81024d7757fbf3db3b270861bcf474916b0dfe
ghofranebenhmaid <ghofranebh90@gmail.com>
2022-05-25 18:32:42 +0200

    🎉  Add project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { GlobalStyles } from './styles/GlobalStyles'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <h1>Hello!!</h1>
+    </div>
+  )
+}
+
+export default App
